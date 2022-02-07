eaffe252161025d126943669b3a3dc9bc49cdabc
JPBanas <jbanas9124@gmail.com>
2022-02-07 11:43:27 -0600

    Testing
    - Testing for Ether deposit fx, events
    
    -Testing for token deposit fx, events

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -1,7 +1,8 @@
-import { formatTokens, EVM_REVERT } from './helpers';
+import { formatTokens, EVM_REVERT, ETHER_ADDRESS, ether } from './helpers';
 
 // eslint-disable-next-line no-undef
 const Exchange = artifacts.require('../src/contracts/Exchange.sol');
+const Token = artifacts.require('../src/contracts/Token.sol');
 require('chai').use(require('chai-as-promised')).should();
 
 /* 
@@ -25,14 +26,19 @@ it('', async () => {
 // smart contract deployment tests (through truffle 'truffle test' in terminal)
 // destructured array variables for ganache created addresses, for testing purposes ERC-20 standards
 // eslint-disable-next-line no-undef
-contract('Exchange', ([deployer, feeAccount]) => {
+contract('Exchange', ([deployer, feeAccount, userOne]) => {
+  let token;
   let exchange;
   const feePercent = 10;
 
   // Fetch Exchange contract
   beforeEach(async () => {
+    token = await Token.new();
+    // give userOne 100 tokens to be able to deposit with an exchange
+    token.transfer(userOne, formatTokens(100));
     // fee account is passed to the smart contract constructor
     exchange = await Exchange.new(feeAccount, feePercent);
+    
   })
 
   // Testing Exchange deployment
@@ -54,4 +60,98 @@ contract('Exchange', ([deployer, feeAccount]) => {
       result.toString().should.equal(feePercent.toString());
     })
   })
+
+  describe('depositing Ether', async () => {
+
+    let result;
+    let amount;
+
+    beforeEach(async () => {
+      amount = ether(1);
+      result = await exchange.depositEther({ from: userOne, value: amount,  })
+    })
+
+    it('tracks the Ether deposit', async () => {
+      const balance = await exchange.tokens(ETHER_ADDRESS, userOne);
+      balance.toString().should.equal(amount.toString());
+    })
+
+    it('emits a Deposit event', async () => {
+      const log = result.logs[0];
+      log.event.should.equal('Deposit');
+
+      const event = log.args;
+      event.token.toString().should.equal(ETHER_ADDRESS, 'The token is correct!');
+      event.user.toString().should.equal(userOne, "The user is correct!");
+      event.amount.toString().should.equal(amount.toString(), "Transfer amount correct!");
+      event.balance.toString().should.equal(amount.toString(), 'New balance is correct!');
+    })
+
+  })
+
+  describe('fallback function', () => {
+    it('revert if Ether is sent', async () => {
+
+      // sendTransaction sends a general ethereum transaction for any amount
+      //  we want to reject because we cannot refund ether, so we revert
+      await exchange.sendTransaction({ value: ether(10), from: userOne }).should.be.rejectedWith(EVM_REVERT)
+
+    })
+  })
+
+  describe('depositing tokens', () => {
+    let result;
+    let amount;
+    
+
+    describe('success', () => {
+
+      beforeEach(async () => {
+        amount = formatTokens(10)
+        await token.approve(exchange.address, amount, { from: userOne });
+        result = await exchange.depositToken(token.address, amount, { from: userOne });
+      })
+
+      it('tracks the token deposit', async () => {
+        // checks exchange token balance
+        let balance;
+        balance = await token.balanceOf(exchange.address)
+        balance.toString().should.equal(amount.toString())
+
+        //checks balance after deposit
+        balance = await exchange.tokens(token.address, userOne);
+        balance.toString().should.equal(amount.toString())
+
+      })
+
+      // Check for event Deposit to trigger.
+      it('emits a Deposit event', async () => {
+        const log = result.logs[0];
+        log.event.should.equal('Deposit');
+
+        const event = log.args;
+        event.token.toString().should.equal(token.address, 'The token is correct!');
+        event.user.toString().should.equal(userOne, "The user is correct!");
+        event.amount.toString().should.equal(amount.toString(), "Transfer amount correct!");
+        event.balance.toString().should.equal(amount.toString(), 'New balance is correct!');
+      })
+    })
+    
+    describe('failure', () => {
+
+      // Don't allow Ether deposits
+      it('rejects Ether deposits', async () => {
+        // cannot deposit ether
+        await exchange.depositToken(ETHER_ADDRESS, formatTokens(10), { from: userOne }).should.be.rejectedWith(EVM_REVERT);
+      })
+
+      it('fails when no tokens are approved', async () => {
+        // don't approve tokens before depositing
+        await exchange.depositToken(token.address, amount, { from: userOne }).should.be.rejectedWith(EVM_REVERT);
+      })
+
+    })
+
+  })
+
 })
