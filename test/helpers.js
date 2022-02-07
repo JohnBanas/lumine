a6df8520fbbd670dec83842dd9601c50a08adaf5
JPBanas <jbanas9124@gmail.com>
2022-02-07 11:40:25 -0600

    Refactor
    - create an format ether to wei fx and a format fx that calls it.
    
    - this helps explicitly track when we are sending our tokens vs Ether

--- test/helpers.js
+++ test/helpers.js
@@ -1,9 +1,13 @@
 const web3 = require('web3');
 //helper fx
-export const formatTokens = (tokenAmount) => {
+export const ether = (tokenAmount) => {
   return new web3.utils.BN(
     web3.utils.toWei(tokenAmount.toString(), 'ether')
   )
 };
+
+export const formatTokens = (n) => ether(n);
+
+export const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 export const EVM_REVERT = 'VM Exception while processing transaction: revert';
