c7bf07b718a196a02e9a2a58bc4dfc11427ed7a8
JPBanas <jbanas9124@gmail.com>
2022-02-07 15:03:13 -0600

    Withdraw functions
    - Withdraw fxs for Ether
    - Withdraw fxs for tokens

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -42,8 +42,9 @@ contract Exchange	{
 		mapping(address => mapping(address => uint256)) public tokens; // track tokens (first address) that have been deposited
 		// second address is the user that has deposited
 
-		//  Define event
+		//  Define events
 		event Deposit(address token, address user, uint256 amount, uint256 balance);
+		event Withdraw(address token, address user, uint256 amount, uint256 balance);
 
 		/* constructor function */
     constructor (address _feeAccount, uint256 _feePercent) public {
@@ -56,12 +57,42 @@ contract Exchange	{
 			revert();
 		}
 
+		// Withdraw Ether
+
+		function withdrawEther(uint256 _amount) public {
+			// check to make sure they have the amount to withdraw
+			require(tokens[ETHER][msg.sender] >= _amount);
+			//update Ether balance in tokens mapping
+			tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].sub(_amount);
+			//send the Ether back to user
+			msg.sender.transfer(_amount);
+			//emit event 
+			emit Withdraw(ETHER, msg.sender, _amount, tokens[ETHER][msg.sender]);
+		}
+
 		// Deposit Ether 
 		function depositEther() payable public {
 			tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].add(msg.value);
 			emit Deposit(ETHER, msg.sender, msg.value, tokens[ETHER][msg.sender]);
 		}
 
+		// Withdraw Token
+		function withdrawToken(address _token, uint256 _amount) public {
+			// TODO: Don't allow Ether withdraw
+			require(_token != ETHER);
+			// check to make sure they have the amount to withdraw
+			require(tokens[_token][msg.sender] >= _amount);
+			// remove tokens from address
+			tokens[_token][msg.sender] = tokens[_token][msg.sender].sub(_amount);
+
+			//require token transfer from contract (implied) to user
+			require(Token(_token).transfer(msg.sender, _amount));
+
+			// emit event
+			emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+		}
+
+		// Deposit Token
 		function depositToken(address _token, uint256 _amount) public {
 			// TODO: Don't allow Ether deposits
 			require(_token != ETHER);
