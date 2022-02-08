1b14f832093b7496b6992f844ab66898a474946b
JPBanas <jbanas9124@gmail.com>
2022-02-08 11:52:34 -0600

    Reset migration
    - Truffle migrate --reset for dev client testing

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,5 @@
-const web3 = require("web3");
+const Web3 = require("web3");
+const web3 = new Web3(Web3.givenProvider || 'HTTP://127.0.0.1:7545');
 
 const Token = artifacts.require("Token");
 const Exchange = artifacts.require("Exchange");
