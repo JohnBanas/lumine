cdafa4fa0e6ecf03c8c6353dfcba6b884aca4af6
JPBanas <jbanas9124@gmail.com>
2022-02-12 19:04:37 -0600

    helpers update

--- test/helpers.js
+++ test/helpers.js
@@ -1,22 +1,12 @@
-const web3 = require('web3');
-//helper fx for formatting ether to wei
-export const ether = (tokenAmount) => {
+export const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+export const EVM_REVERT = 'VM Exception while processing transaction: revert'
+
+export const ether = (n) => {
   return new web3.utils.BN(
-    web3.utils.toWei(tokenAmount.toString(), 'ether')
+    web3.utils.toWei(n.toString(), 'ether')
   )
-};
-
-// calls ether helper fx, this is to help explicitly identify when we are
-//  testing for ether and when we are testing our tokens
-export const formatTokens = (n) => ether(n);
-
-// Ether address for mapping
-export const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000';
-
-// Variable to hold the revert returns
-export const EVM_REVERT = 'VM Exception while processing transaction: revert';
-
-export const wait = (n) => {
-  const milliseconds = n * 1000;
-  return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
+
+// Same as ether
+export const tokens = (n) => ether(n)
