58026d7fc2aa53d2cc6643e44790ec6605fe8d99
JPBanas <jbanas9124@gmail.com>
2022-02-12 18:58:53 -0600

    redo migration files delete unnecessary react files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,16 +1,13 @@
-const Web3 = require("web3");
-const web3 = new Web3(Web3.givenProvider || 'HTTP://127.0.0.1:7545');
-
 const Token = artifacts.require("Token");
 const Exchange = artifacts.require("Exchange");
 
-module.exports = async function (deployer) {
-  const accounts = await web3.eth.getAccounts();
+module.exports = async function(deployer) {
+  const accounts = await web3.eth.getAccounts()
 
   await deployer.deploy(Token);
 
-  const feeAccount = accounts[0];
-  const feePercentage = 10;
+  const feeAccount = accounts[0]
+  const feePercent = 10
 
-  await deployer.deploy(Exchange, feeAccount, feePercentage);
+  await deployer.deploy(Exchange, feeAccount, feePercent)
 };
